44c5858f5b6e5ca6bc216b052b5c001eb8f70269
mengtiah <46981320+mengtiah@users.noreply.github.com>
2020-02-25 00:13:16 -0800

    Create Finding Extreme Points-159.py

---
+++ calculus-for-machine-learning/Finding Extreme Points-159.py
@@ -0,0 +1,26 @@
+## 3. Differentiation ##
+
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.linspace(-5,6,110)
+y=-2*x+3
+plt.plot(x,y)
+
+## 6. Power Rule ##
+
+slope_one=16*5
+slope_two=0
+
+## 7. Linearity Of Differentiation ##
+
+slope_three=4
+slope_four=3*4-4
+
+## 8. Practicing Finding Extreme Values ##
+
+rel_max=[]
+rel_min=[]
+
+   
+rel_max.append(0)
+rel_min.append(2/3)
