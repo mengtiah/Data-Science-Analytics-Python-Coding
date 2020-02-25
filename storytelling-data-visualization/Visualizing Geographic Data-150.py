9e5970f6716393667ffbbc0c7d15769536c8beaa
mengtiah <46981320+mengtiah@users.noreply.github.com>
2020-02-25 00:12:18 -0800

    Add files via upload

---
+++ storytelling-data-visualization/Visualizing Geographic Data-150.py
@@ -0,0 +1,80 @@
+## 1. Geographic Data ##
+
+import pandas as pd
+airlines=pd.read_csv('airlines.csv')
+airports=pd.read_csv('airports.csv')
+routes=pd.read_csv('routes.csv')
+print(airlines.iloc[0,])
+print(airports.iloc[0,])
+print(routes.iloc[0,])
+
+
+## 4. Workflow With Basemap ##
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+m=Basemap(projection= "merc",llcrnrlat= -80,urcrnrlat=80,llcrnrlon=-180,urcrnrlon=180)
+
+## 5. Converting From Spherical to Cartesian Coordinates ##
+
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+x, y = m(airports['longitude'].tolist(), airports['latitude'].tolist())
+
+## 6. Generating A Scatter Plot ##
+
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+x, y = m(longitudes, latitudes)
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+x, y = m(longitudes, latitudes)
+m.scatter(x, y, s=1)
+plt.show()
+
+## 7. Customizing The Plot Using Basemap ##
+
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+longitudes = airports["longitude"].tolist()
+latitudes = airports["latitude"].tolist()
+x, y = m(longitudes, latitudes)
+m.scatter(x, y, s=1)
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+longitudes = airports["longitude"].tolist()
+latitudes = airports["latitude"].tolist()
+x, y = m(longitudes, latitudes)
+m.scatter(x, y, s=1)
+m.drawcoastlines()
+plt.show()
+
+## 8. Customizing The Plot Using Matplotlib ##
+
+# Add code here, before creating the Basemap instance.
+fig, ax = plt.subplots(figsize=(15,20))
+ax.set_title('Scaled Up Earth With Coastlines')
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+longitudes = airports["longitude"].tolist()
+latitudes = airports["latitude"].tolist()
+x, y = m(longitudes, latitudes)
+m.scatter(x, y, s=1)
+m.drawcoastlines()
+plt.show()
+
+## 9. Introduction to Great Circles ##
+
+geo_routes=pd.read_csv('geo_routes.csv')
+geo_routes.info()
+geo_routes.head()
+
+## 10. Displaying Great Circles ##
+
+fig, ax = plt.subplots(figsize=(15,20))
+m = Basemap(projection='merc', llcrnrlat=-80, urcrnrlat=80, llcrnrlon=-180, urcrnrlon=180)
+m.drawcoastlines()
+
+def create_great_circles(df):
+    for index,row in df.iterrows():
+        if abs(row[7]- row[6]) < 180:
+            if abs(row[5]- row[4]) < 180:
+                m.drawgreatcircle(row[4],row[6],row[5],row[7])
+dfw=geo_routes[geo_routes['source']=='DFW']
+create_great_circles(dfw)
+plt.show()
+       
